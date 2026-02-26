877e3c9b91fd2aaf227a7a5480ad4ffaa35c16b6
ainagaya <aina.gaya-avila@tum.de>
2026-02-26 15:41:53 +0100

    plot ics

--- plot_ics.py
+++ plot_ics.py
@@ -1,32 +1,34 @@
 import torch
 import matplotlib.pyplot as plt
 
-d = torch.load("sample_000.pt", map_location="cpu")
-X, Y = d["X"], d["Y"]
+for i in range(17):
 
-print("X shape:", tuple(X.shape))
-print("Y shape:", tuple(Y.shape))
+    d = torch.load(f"train_sample_{i}.pt", map_location="cpu")
+    X, Y = d["X"], d["Y"]
 
-# normalize shapes to (T, C, H, W)
-X_tc = X[0] if X.ndim == 5 else X
-Y_tc = Y[0] if Y.ndim == 5 else Y
+    print("X shape:", tuple(X.shape))
+    print("Y shape:", tuple(Y.shape))
 
-t = 0
-c = 0
-x_img = X_tc[t, c].numpy()
-y_img = Y_tc[t, c].numpy()
+    # normalize shapes to (T, C, H, W)
+    X_tc = X[0] if X.ndim == 5 else X
+    Y_tc = Y[0] if Y.ndim == 5 else Y
 
-plt.figure(figsize=(10, 4))
+    t = 0
+    c = 0
+    x_img = X_tc[t, c].numpy()
+    y_img = Y_tc[t, c].numpy()
 
-plt.subplot(1, 2, 1)
-plt.title(f"X (t={t}, c={c})")
-plt.imshow(x_img, origin="lower")
-plt.colorbar(fraction=0.046, pad=0.04)
+    plt.figure(figsize=(10, 4))
 
-plt.subplot(1, 2, 2)
-plt.title(f"Y (t={t}, c={c})")
-plt.imshow(y_img, origin="lower")
-plt.colorbar(fraction=0.046, pad=0.04)
+    plt.subplot(1, 2, 1)
+    plt.title(f"X (t={t}, c={c})")
+    plt.imshow(x_img, origin="lower")
+    plt.colorbar(fraction=0.046, pad=0.04)
 
-plt.tight_layout()
-plt.show()
+    plt.subplot(1, 2, 2)
+    plt.title(f"Y (t={t}, c={c})")
+    plt.imshow(y_img, origin="lower")
+    plt.colorbar(fraction=0.046, pad=0.04)
+
+    plt.tight_layout()
+    plt.savefig(f"train_sample_{i}_t{t}_c{c}.png")
